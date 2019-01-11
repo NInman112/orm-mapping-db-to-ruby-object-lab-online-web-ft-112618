ae03d8a51f5b341456117b2c8a1a8ea844f1a73d
Nick Inman <nick@theoatmeal.com>
2019-01-11 00:24:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,7 @@ class Student
   end
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT COUNT(?) FROM students WHERE grade = 10
+      SELECT FROM students WHERE grade = 10 LIMIT (?)
     SQL
 
     DB[:conn].execute(sql, x).map do |row|
